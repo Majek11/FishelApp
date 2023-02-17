30a6f0a0dc84dad979ef2fb748da59a9e504300b
Hemephelus <nwachukwuujubuonu@gmail.com>
2023-02-17 03:26:46 +0100

    data

--- src/layouts/SelectModePage.jsx
+++ src/layouts/SelectModePage.jsx
@@ -7,18 +7,19 @@ import { Link } from 'react-router-dom'
 
 const SelectModePage = () => {
   return (
-    <div className="min-h-screen lg:p-16 bg-[url('/public/images/MainImage.png')] bg-cover bg-center text-[#E3F1F4]">
-        <div className="bg-slate-300 w-full h-full"></div>
-    <div className='flex flex-col gap-8 w-full h-full bg-slate-500'>
+    <div className="h-screen  bg-[url('/public/images/MainImage.png')] bg-cover bg-center bg-no-repeat text-[#E3F1F4]">
+        <div className=" w-full h-full bg-[#353c3e4b]  flex flex-col gap-8 justify-center items-center">
+
+    <div className='flex flex-col gap-4 p-4 lg:gap-8 lg:p-8'>
  <div className=' space-y-1'>
  <h1 className='text-3xl'>What mode are you using?</h1>
-        <div className='bg-[#E3F1F4] h-[1px] w-full'></div>
+        <div className='bg-[#E3F1F4] h-[2px] w-full'></div>
  </div>
- <div className='flex gap-8'>
+ <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 lg:justify-between lg:gap-0'>
     <Link>
-    <div className="relative lg:w-[320px] rounded lg:h-[225px] bg-[url('/src/assets/create_mode.png')]  hover:border-solid hover:border-2  hover:border-[#e3f1f4] delay-300">
+    <div className=" lg:w-[320px]  rounded-lg lg:rounded lg:h-[225px] bg-[url('/src/assets/create_mode.png')] bg-cover bg-no-repeat bg-center hover:border-solid hover:border-2  hover:border-[#e3f1f4] delay-300">
      
-        <div className='h-full w-full bg-[#353c3ea4] cursor-pointer p-6 hover:bg-[#353c3ee6] duration-300 delay-300'>
+        <div className=' h-[150px] lg:h-full lg:w-full bg-[#353c3ea4] rounded-lg cursor-pointer p-6 hover:bg-[#353c3ee6] duration-300 delay-300'>
         <div className=' space-y-2'>
             <h1 className='font-bold text-2xl'>
             Create Mode
@@ -32,8 +33,9 @@ const SelectModePage = () => {
     </div>
     </Link>
     <Link>
-    <div className="relative lg:w-[320px] rounded lg:h-[225px] bg-[url('/src/assets/print_mode.png')]  hover:border-solid hover:border-2  hover:border-[#e3f1f4] delay-300">
-        <div className='h-full w-full bg-[#353c3ea4] cursor-pointer p-6 hover:bg-[#353c3ee6] duration-300 delay-300'>
+    <div className=" lg:w-[320px]  rounded-lg lg:rounded lg:h-[225px] bg-[url('/src/assets/print_mode.png')] bg-cover bg-no-repeat bg-center hover:border-solid hover:border-2  hover:border-[#e3f1f4] delay-300">
+     
+     <div className=' h-[150px] lg:h-full lg:w-full bg-[#353c3ea4] rounded-lg cursor-pointer p-6 hover:bg-[#353c3ee6] duration-300 delay-300'>
         <div className=' space-y-2'>
             <h1 className='font-bold text-2xl'>
             Print Mode
@@ -47,8 +49,9 @@ const SelectModePage = () => {
     </div>
     </Link>
     <Link>
-    <div className="relative lg:w-[320px] rounded lg:h-[225px] bg-[url('/src/assets/answer_online_mode.png')]  hover:border-solid hover:border-2  hover:border-[#e3f1f4] delay-300">
-        <div className='h-full w-full bg-[#353c3ea4] cursor-pointer p-6 hover:bg-[#353c3ee6] duration-300 delay-300'>
+    <div className=" lg:w-[320px]  rounded-lg lg:rounded lg:h-[225px] bg-[url('/src/assets/answer_online_mode.png')] bg-cover bg-no-repeat bg-center hover:border-solid hover:border-2  hover:border-[#e3f1f4] delay-300">
+     
+     <div className=' h-[150px] lg:h-full lg:w-full bg-[#353c3ea4] rounded-lg cursor-pointer p-6 hover:bg-[#353c3ee6] duration-300 delay-300'>
         <div className=' space-y-2'>
             <h1 className='font-bold text-2xl'>
             Answer Online Mode
@@ -63,6 +66,7 @@ const SelectModePage = () => {
     </Link>
  
  </div>
+        </div>
         </div>
     </div>
   )
